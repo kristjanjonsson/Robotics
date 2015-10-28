57cb844be02f2990a7dd09a089d4a4255aacb899
Kristjan Jonsson <kristjan.jonsson88@gmail.com>
2015-10-27 22:35:27 -0700

    Integrated into motion node. Doesn't work on my machine.

--- src/motion.py
+++ src/motion.py
@@ -3,38 +3,46 @@
 import rospy
 from A53070542_assignment_4.srv import KeyboardService
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+from bbox import OpticalFlow, MOG2
 
 
 class Motion:
 
     def __init__(self, node_name):
         rospy.init_node(node_name)
+        self.bridge = CvBridge()
         self.pub = rospy.Publisher('/camera/visible/image', Image, queue_size=10)
         rospy.Subscriber("camera/image_raw", Image, self.imageCallback)
-        s = rospy.Service('motion_mode_keyboard', KeyboardService, self.printer)
-        self.mode = None
+        s = rospy.Service('motion_mode_keyboard', KeyboardService, self.change_mode)
+        self.motion_detector = None
         s.spin()
 
-    def printer(self, request):
-        if request.mode not in 'rfm':
-            return 'Invalid mode {0}'.format(request.mode)
-        print "Mode set to ", request.mode
-        self.mode = request.mode
+    def change_mode(self, request):
+        try:
+            if request.mode == 'r':
+                self.motion_detector = None
+            elif request.mode == 'f':
+                self.motion_detector = OpticalFlow()
+            elif request.mode == 'm':
+                self.motion_detector = MOG2()
+            else:
+                raise ValueError('Invalid mode: {0}'.format(request.mode))
+        except Exception as e:
+            return str(e)
+
+        print 'Mode set to ' + request.mode
+        return ''
 
     def imageCallback(self, image):
-        print "Get the image."
+        if self.motion_detector:
+            cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+            self.motion_detector.apply(cv_image)
+            self.motion_detector.draw_bboxes(cv_image)
+            image = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
 
-        if self.mode == 'r':
-            print "Publish image to /camera/visible/image"
-            self.pub.publish(image)
-        
-        elif self.mode == 'f':
-            # TODO
-            pass
-        
-        elif self.mode == 'm':
-            # TODO
-            pass
-        
+        self.pub.publish(image)
+
 if __name__ == "__main__":
     motion = Motion('motion_node')
